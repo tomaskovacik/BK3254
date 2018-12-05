1db4ad55419cfec47e6248a1f3f121e1f754e327
Tomas kovacik <nail@nodomain.sk>
2018-12-05 18:25:39 +0100

    position of comment was wrong

--- src/BK3254.cpp
+++ src/BK3254.cpp
@@ -74,8 +74,6 @@ uint8_t BK3254::getNextEventFromBT() {
   delay(200);//delay needed to fillup buffers otherwise this will return false
   while (btSerial -> available() > 0 && c != '\n') { //read serial buffer until \n
     c = (btSerial -> read());
-    //append received buffer with received character
-
     if (c == 0xA) {
       if (receivedString == "") { //nothing before enter was received
   #if defined DEBUG
@@ -85,6 +83,7 @@ uint8_t BK3254::getNextEventFromBT() {
       }
       return decodeReceivedString(receivedString);
     }
+    //append received buffer with received character
     if (c != 0xD) receivedString += c;  // skip \r
   }
   return 0;
